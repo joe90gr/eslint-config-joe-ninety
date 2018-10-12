5cdf69cc864b4bb3a3840c6f5075241b00c040af
joe ruggieri <joe90joe40@hotmail.com>
2018-10-12 01:02:15 +0100

    feat: creates new eslint module for projects

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    globals: {
+        Raven: true
+    },
+    plugins: [ 'react', 'import', 'lodash', 'promise' ],
+    rules: {}
+}
